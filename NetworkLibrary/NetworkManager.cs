71e5fb55b520328428ca3f130d16489fee1bc755
GuoHao Zeng <guohao.zeng@winwayglobal.com>
2021-11-23 17:18:08 +0800

    <fix> Fix rule for CheckIntranetOrInternet method

--- NetworkLibrary/NetworkManager.cs
+++ NetworkLibrary/NetworkManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 namespace CodeFoxx.NetworkLibrary
 {
@@ -11,53 +11,33 @@ namespace CodeFoxx.NetworkLibrary
 
         public static NetType CheckIntranetOrInternet(string targetIP)
         {
+            string[] targetIPSections = targetIP.Split('.');
+
             /// IP地址中，有三段地址專門用於私網的規劃，不能被用於網際網路上的連線如下
             /// Class A：10.0.0.0-10.255.255.255
-            string intranetAClassFirstSection = "10.";
-            List<string> intranetAClassHalfSection = new List<string>();
-            for (int i = 0; i <= 255; ++i)
+            if (targetIPSections[0] == "10")
             {
-                intranetAClassHalfSection.Add(intranetAClassFirstSection + i);
+                return NetType.INTRANET;
             }
 
             /// Class B：172.16.0.0-172.31.255.255
-            string intranetBClassFirstSection = "172.";
-            List<string> intranetBClassHalfSection = new List<string>();
-            for (int i = 16; i <= 31; ++i)
+            if (targetIPSections[0] == "172")
             {
-                intranetBClassHalfSection.Add(intranetBClassFirstSection + i);
-            }
-
-            /// Class C類：192.168.0.0-192.168.255.255
-            string intranetCClass = "192.168";
-
-            List<string> intranetIPs = new List<string>();
-            intranetIPs.AddRange(intranetAClassHalfSection);
-            intranetIPs.AddRange(intranetBClassHalfSection);
-            intranetIPs.Add(intranetCClass);
-
-            foreach (var intranetIP in intranetIPs)
-            {
-                if (targetIP.StartsWith(intranetIP))
+                int targetIPSecondSection = Convert.ToInt16(targetIPSections[1]);
+                if (targetIPSecondSection >= 16 && targetIPSecondSection <= 31)
                 {
                     return NetType.INTRANET;
                 }
             }
 
+            /// Class C：192.168.0.0-192.168.255.255
+            if (targetIPSections[0] == "192" && targetIPSections[1] == "168")
+            {
+                return NetType.INTRANET;
+            }
+
             return NetType.INTERNET;
 
-            //for backup from survey
-            //string ip = Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != null ? Request.ServerVariables["HTTP_X_FORWARDED_FOR"] : Request.UserHostAddress;
-            //Debug.WriteLine($"ip = {ip}");
-            //if (ip.IndexOf("192.168") > -1)
-            //{
-            //    //內網
-            //    headingBackgroundManagement.Visible = true;
-            //}
-            //else
-            //{
-            //    //外網
-            //}
         }
     }
 }
