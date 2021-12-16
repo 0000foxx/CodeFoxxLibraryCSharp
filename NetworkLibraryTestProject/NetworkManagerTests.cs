d998fe0cb85b10a588a33f48696cef782aa8b1ce
0000foxx <0000foxx@gmail.com>
2021-12-16 18:10:25 +0800

    <feat> Add NetworkLibraryTestProject
    Reason:
    For test NetworkLibrary
    
    Range:
    NetworkLibraryTestProject
    
    Notice:
    None

---
+++ NetworkLibraryTestProject/NetworkManagerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CodeFoxxLibrary.NetworkLibrary;
+using NUnit.Framework;
+
+namespace NetworkLibraryTestProject
+{
+    [TestFixture]
+    public class NetworkManagerTests
+    {
+        [Test]
+        public void CheckIntranetOrInternet_Intranet_ReturnTrue()
+        {
+            NetworkManager.NetType expectIntranet = NetworkManager.CheckIntranetOrInternet("192.168.1.1");
+            Assert.AreEqual(expectIntranet, NetworkManager.NetType.INTRANET);
+        }
+    }
+}
